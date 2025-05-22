344510ec934edb2e3f4c97c9cd3874758eca3d44
unknown <abdullahzaid2904@gmail.com>
2025-05-22 19:00:05 +0530

    feat: reusable components added

--- frontend/src/components/ui/index.js
+++ frontend/src/components/ui/index.js
@@ -2,5 +2,10 @@ export { default as Card } from './Card';
 export { default as Badge } from './Badge';
 export { default as Button } from './Button';
 export { default as Select } from './Select';
+export { default as Modal } from './Modal';
+export { default as Spinner } from './Spinner';
+export { default as Table } from './Table';
+
+
 
 
